50cd2b0c5f4c07448ad1418a3a9965d97d81f87b
Mouss AID <mousab.aidoud@ext.adeo.com>
2024-05-09 16:49:04 +0200

    init (dbt) : add third party packages

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -5,13 +5,14 @@ on_schema_change='fail'
 )
 }}
 WITH src_reviews AS (
-    SELECT * FROM {{ ref('src_reviews') }}
+SELECT * FROM {{ ref('src_reviews') }}
 )
-
-SELECT * FROM src_reviews
-WHERE
-    review_text IS NOT null
-
-    {% if is_incremental() %}
-        AND review_date > (SELECT max(review_date) FROM {{ this }})
-    {% endif %}
+SELECT
+{{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }}
+AS review_id,
+*
+FROM src_reviews
+WHERE review_text is not null
+{% if is_incremental() %}
+AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
